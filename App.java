ed1eb5a7039d8ee4165ec660da0b062a6bba9316
Szymon Leyk <biuro@szymonleyk.pl>
2020-12-04 15:07:19 +0100

    zadania z rozdziału o zmiennych

---
+++ App.java
@@ -0,0 +1,18 @@
+class App {
+	public static void main (String args[]){
+		// zadanie 1
+		System.out.println("------- Zadanie 1 -------");
+		int birthYear = 2001;
+		char gender = 'K';
+		
+		System.out.println("Birth year: "+birthYear);
+		System.out.println("Gender: "+gender);
+		
+		// zadanie 2
+		System.out.println("------- Zadanie 2 -------");
+		boolean isAdult = true;
+		double average = 3.5;
+		System.out.println("Is adult? "+isAdult);
+		System.out.println("Average: "+average);
+	}
+}
